b95428a7c12e69213d662cf2f3ef18a80272ee71
Alain Tran <alaint77@gmail.com>
2019-04-12 20:38:41 +0200

    commit basic files with consigne

---
+++ app.js
@@ -0,0 +1,5 @@
+// It should have a place to store todos
+// It should have a way to display todos
+// It should have a way add new todos
+// It should have a way to change todos
+// It should have a way to delete a todo
