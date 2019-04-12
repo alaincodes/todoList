e073cda60167d823d087cfa2b8f93d7ba0b9b16a
Alain Tran <alaint77@gmail.com>
2019-04-12 22:39:56 +0200

    Changed entire todo to an Object!

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-// It should have a place to store todos
-let todos = ['item1', 'item2', 'item3', 1990];
+let todoList = {
+  // It should have a place to store todos
+  todos: ["item1", "item2", "item3", 1990],
 
-// It should have a way to display todos
-function displayTodos() {
-  console.log(`My todos : ${todos}`);
-}
+  // It should have a way to display todos
+  displayTodos: function() {
+    console.log(`My todos : ${this.todos}`);
+  },
 
-// It should have a way add new todos
-function addTodos(newValue) {
-  todos.push(newValue);
-  displayTodos();
-}
+  // It should have a way add new todos
+  addTodos: function(newValue) {
+    this.todos.push(newValue);
+    this.displayTodos();
+  },
 
-// It should have a way to change todos
-function changeTodos(index, newValue) {
-  todos[index] = newValue;
-  displayTodos();
-}
+  // It should have a way to change todos
+  changeTodos: function(index, newValue) {
+    this.todos[index] = newValue;
+    this.displayTodos();
+  },
 
-// It should have a way to delete a todo
-function deleteTodos(index, deleteValue) {
-  todos.splice(index, deleteValue);
-  displayTodos();
-}
+  // It should have a way to delete a todo
+  deleteTodos: function(index, deleteValue) {
+    this.todos.splice(index, deleteValue);
+    this.displayTodos();
+  }
+};
